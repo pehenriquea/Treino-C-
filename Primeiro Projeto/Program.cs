b234132de61a882a24f81e4c4d4c40d2ef0cbb85
pedro.santos <pedro.santos@PE0363AE-NB>
2024-08-07 15:56:07 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro Projeto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro_Projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string val;
+            int y, fat;
+            Program obj;
+
+            Console.WriteLine("Digite o número desejado: ");
+            val = Console.ReadLine();
+
+            if ((int.TryParse(val.ToString(), out y)))
+            {
+                obj = new Program();
+                fat = obj.CalculaFat(y);
+                Console.WriteLine($"Resultado: {fat}");
+            }
+
+            Console.ReadLine();
+
+        }
+        public int CalculaFat(int x)
+        {
+            if (x == 0)
+                return 1;
+            else
+                return x * CalculaFat(x - 1);
+        }
+    }
+}
